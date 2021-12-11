7d1d57cf6b83b4637fd751216b79f226e1eb23ba
Pavel.K <pavelweb74@gmail.com>
2021-12-11 19:35:39 +0500

    Connect express

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import { config } from "dotenv";
+import express from "express";
 
 config();
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+  console.log(`SERVER started on port ${port}`);
+});
