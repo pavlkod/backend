049d9d4d2ab77f9ec2cb2f7d453cb239e3fa4c97
Pavel.K <pavelweb74@gmail.com>
2021-12-12 15:39:02 +0500

    Add post to DB

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 import express from "express";
+
+import Post from "../models/Post.js";
+
 const router = new express();
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.status(200).json("OK");
+router.post("/", async (req, res) => {
+  const { author, title, content, picture } = req.body;
+  const post = await Post.create({ author, title, content, picture });
+  res.json(post);
 });
 
 export default router;
