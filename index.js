790f5401d6534e5bb35d3d61f6a84cb0f2dfbc66
Pavel.K <pavelweb74@gmail.com>
2021-12-12 14:19:07 +0500

    Connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { config } from "dotenv";
+
 import express from "express";
+import mongoose from "mongoose";
 
 import router from "./routes/index.js";
 
@@ -8,8 +10,17 @@ config();
 const app = express();
 const port = process.env.PORT || 3030;
 
+app.use(express.json());
 app.use("/", router);
 
-app.listen(port, () => {
-  console.log(`SERVER started on port ${port}`);
-});
+const startApp = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(port, () => {
+      console.log(`SERVER started on port ${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+startApp();
