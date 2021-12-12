2b9ed7f11f15d686f818fb5d215a9c39a9a6285e
Pavel.K <pavelweb74@gmail.com>
2021-12-12 14:04:25 +0500

    Add post hanlder

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router = new express();
 router.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).json("OK");
+});
 
 export default router;
