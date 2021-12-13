b65aed338e58d47a11e0f5836d383a20cff2e45f
Pavel.K <pavelweb74@gmail.com>
2021-12-13 23:15:34 +0500

    Add IIFE start function

--- index.js
+++ index.js
@@ -8,19 +8,17 @@ import router from "./routes/index.js";
 config();
 
 const app = express();
-const port = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use("/", router);
 
-const startApp = async () => {
+(async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
-    app.listen(port, () => {
-      console.log(`SERVER started on port ${port}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`SERVER started on port ${process.env.PORT}`);
     });
   } catch (e) {
     console.log(e);
   }
-};
-startApp();
+})();
