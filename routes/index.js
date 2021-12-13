ddeeb6a4c9d675bd92a2a098b9887c9de8e3cd22
Pavel.K <pavelweb74@gmail.com>
2021-12-13 23:22:00 +0500

    Refactor routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 import express from "express";
 
-import Post from "../models/Post.js";
+import MainRouter from "./main.js";
+import PostRouter from "./posts.js";
 
 const router = new express();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "OK" });
-});
-router.post("/", async (req, res) => {
-  const { author, title, content, picture } = req.body;
-  const post = await Post.create({ author, title, content, picture });
-  res.json(post);
-});
+router.use("/", MainRouter);
+router.use("/posts", PostRouter);
 
 export default router;
