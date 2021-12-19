1870159e6a62dbc2981e2d6c8822da81f817f3fc
Pavel.K <pavelweb74@gmail.com>
2021-12-19 13:42:30 +0500

    Add FileService

---
+++ service/File.js
@@ -0,0 +1,16 @@
+import * as uuid from "uuid";
+import * as path from "path";
+
+class FileService {
+  saveFile(file) {
+    try {
+      const fileName = `${uuid.v4()}.jpeg`;
+      const filePath = path.resolve("assets", "img", fileName);
+      file.mv(filePath);
+      return fileName;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+export default new FileService();
